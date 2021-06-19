146f9a5d367567973eb2c457c5ab4ae1ad1800fe
Arunav Sen <arunav.sen@dimikit.com>
2021-06-20 00:36:18 +0600

    Create Course Controller and View

---
+++ DimikInfoProject/Controllers/CourseController.cs
@@ -0,0 +1,35 @@
+﻿using DimikInfoProject.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DimikInfoProject.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CourseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.PageUrl = this.ControllerContext.RouteData.Values["controller"].ToString();
+
+            return View();
+        }
+
+
+        public PartialViewResult CourseList()
+        {
+            ViewBag.PageUrl = this.ControllerContext.RouteData.Values["controller"].ToString();
+
+            var courseList = _db.Courses.ToList();
+            return PartialView(courseList);
+        }
+
+    }
+}
