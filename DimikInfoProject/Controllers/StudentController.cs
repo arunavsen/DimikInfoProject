b73f20c88c4daf45127972374e4c2f8da304678b
Arunav <arunav.sen@dimikit.com>
2021-06-20 17:18:27 +0600

    Add Student Controller

---
+++ DimikInfoProject/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using DimikInfoProject.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DimikInfoProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public StudentController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var studentList = _db.Students.ToList();
+            return View(studentList);
+        }
+    }
+}
